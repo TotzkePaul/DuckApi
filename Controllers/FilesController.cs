5b18bb99b284854b95f88e85f5028cc65a3356a2
paul totzke <7072k3@gmail.com>
2022-07-09 23:27:09 -0500

    csv upload

---
+++ Controllers/FilesController.cs
@@ -0,0 +1,60 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using DuckApi.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+
+namespace DuckApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilesController : ControllerBase
+    {
+        private readonly ILogger<DucksController> _logger;
+        private readonly DataContext _db;
+
+        public FilesController(ILogger<DucksController> logger, DataContext db)
+        {
+            _logger = logger;
+            _db = db;
+        }
+
+        [HttpPost]
+        public IActionResult UploadCSV(IFormFile file)
+        {
+            _logger.LogInformation(file.FileName);
+            var fileExtension = Path.GetExtension(file.FileName);
+            if(fileExtension != ".csv")
+            {
+                return BadRequest($"Non csv extension. Uploaded extension: {fileExtension}");
+            }
+
+
+            var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                HeaderValidated = null,
+                MissingFieldFound = null
+            };
+
+            using (var reader = new StreamReader(file.OpenReadStream()))
+            using (var csvReader = new CsvReader(reader, config))
+            {
+
+                var ducksToUpload = csvReader.GetRecords<Duck>().ToList();
+                var uploadTime = DateTime.Now;
+
+                ducksToUpload.ForEach(d =>
+                {
+                    d.CreateDate = uploadTime;
+                    d.LastModified = uploadTime;
+                });
+
+                _db.AddRange(ducksToUpload);
+
+                _db.SaveChanges();
+            }
+
+            return Ok("Success");
+        }
+    }
+}
