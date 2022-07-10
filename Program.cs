9bc102c9c466ce771b1d8ca392e6bf8a30594624
paul totzke <7072k3@gmail.com>
2022-07-10 00:45:44 -0500

    add cors for local dev

--- Program.cs
+++ Program.cs
@@ -2,12 +2,25 @@ using DuckApi.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://duckapi.azurewebsites.net",
+                              "https://localhost:4200",
+                              "http://localhost:4200");
+                      });
+});
 
 builder.Services.AddSingleton<DataContext>();
 
@@ -18,7 +31,7 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
 
-
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
 
